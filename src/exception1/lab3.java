186b2d5b15d8c41d3b4c3da0ef85b9b24cb76c60
Jittipat007 <jittipat349@gmail.com>
2023-03-28 12:19:06 +0700

    Exception1.1

--- src/exception1/lab3.java
+++ src/exception1/lab3.java
@@ -9,5 +9,17 @@ package exception1;
  * @author Jittipat
  */
 public class lab3 {
-    
+    public static void main(String[] args) {
+        try {
+            System.out.println("Statement 1");
+            System.out.println(list[10]);
+            System.out.println("Statement 3");
+        }
+        catch (Exception e){
+        }
+        finally {
+            System.out.println("Statement 4");
+        }
+        System.out.println("Statement 5");
+    }
 }
